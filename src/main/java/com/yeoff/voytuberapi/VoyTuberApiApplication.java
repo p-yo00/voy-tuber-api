82809fa3d2da8fe471869a963aaebe540f318cfa
yeon <p-yo00@naver.com>
2025-03-05 23:30:14 +0900

    initial test commit

---
+++ src/main/java/com/yeoff/voytuberapi/VoyTuberApiApplication.java
@@ -0,0 +1,13 @@
+package com.yeoff.voytuberapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyTuberApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VoyTuberApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/yeoff/voytuberapi/VoyTuberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeoff.voytuberapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyTuberApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
